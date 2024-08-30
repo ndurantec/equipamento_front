489a9619e9d2c860b8c5248c780934d75da339ef
gelo200815 <eric.reolon@escola.pr.gov.br>
2024-08-30 12:14:22 -0300

    criei a função javascript manutenção

--- script/manutencao.js
+++ script/manutencao.js
@@ -1,3 +1,44 @@
 function salvar() {
-    console.log('Chegou no javascript')
+    const nome_manutencao = document.getElementById('nome');
+   
+  
+    var headers = new Headers();    
+    headers.append("Content-Type", "application/json");
+    headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  
+    fetch('localhost:8080/manutencao/inserir' ,{
+  
+      method: "POST",
+      mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+      cache: "no-cache",
+     
+      // Convertendo o objeto JavaScript para JSON
+      // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+      body: JSON.stringify({ nome: nome_manutencao }),
+  
+      headers: headers
+  
+      //Aqui inicia função then
+    }).then(response => {
+  
+      if(response.ok) {
+  
+        //Esta linha imprime a mensagem no concole
+        console.log('Foi no servidor e voltou');
+  
+        //Esta linha carrega a página sucesso
+        window.location.href = 'sucesso.html'    
+      } else {
+        //Esta linha imprime a mensagem no console
+        console.log('Aconteceu algo que não foi possivel salvar');
+  
+        //Esta linha imprime a mensagem de erro
+        throw new Error('Erro ao tentar salvar');
+      }
+  
+    })
+    //Aqui será executado caso a then não seja chamado
+    .catch(error => console.error('Erro!:', error));
+     
+  
 }
