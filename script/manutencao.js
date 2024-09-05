9627cce6df8c446402245780299956fe9d407136
gelo200815 <eric.reolon@escola.pr.gov.br>
2024-09-05 09:24:11 -0300

    alterações js, html, css

--- script/manutencao.js
+++ script/manutencao.js
@@ -1,44 +1,54 @@
-function salvar() {
-    const nome_manutencao = document.getElementById('nome');
-   
+function salvar_1() {
+  const nome_respon = document.getElementById('nome_respon');
+  const email = document.getElementById('email');
+  const telefone = document.getElementById('telefone');
+  const data = document.getElementById('data');
+  const custo = document.getElementById('custo');   
   
-    var headers = new Headers();    
-    headers.append("Content-Type", "application/json");
-    headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   
-    fetch('localhost:8080/manutencao/inserir' ,{
+  fetch('localhost:8080//inserir' ,{
   
-      method: "POST",
-      mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
-      cache: "no-cache",
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
      
-      // Convertendo o objeto JavaScript para JSON
-      // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
-      body: JSON.stringify({ nome: nome_manutencao }),
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ 
+      nome_respon: nome_respon,
+      email: email,
+      telefone: telefone,
+      data: data,
+      custo: custo,
+      local: local,
+      equipamento: equipamento,
+    }),
+
+    headers: headers
   
-      headers: headers
+    //Aqui inicia função then
+  }).then(response => {
   
-      //Aqui inicia função then
-    }).then(response => {
+    if(response.ok) {
   
-      if(response.ok) {
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
   
-        //Esta linha imprime a mensagem no concole
-        console.log('Foi no servidor e voltou');
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
   
-        //Esta linha carrega a página sucesso
-        window.location.href = 'sucesso.html'    
-      } else {
-        //Esta linha imprime a mensagem no console
-        console.log('Aconteceu algo que não foi possivel salvar');
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
   
-        //Esta linha imprime a mensagem de erro
-        throw new Error('Erro ao tentar salvar');
-      }
-  
-    })
-    //Aqui será executado caso a then não seja chamado
-    .catch(error => console.error('Erro!:', error));
-     
-  
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+
 }
