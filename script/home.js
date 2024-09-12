904c44e150cda83e89a564492ad80563ec96f6ad
abnerolimor <abner.oliveira.moraes@escola.pr.gov.br>
2024-09-12 09:22:15 -0300

    alterei o css

---
+++ script/home.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.getElementById('contabt');
+
+    // Define o destino do link dinamicamente
+    link.href = 'equipamento.html'; // Substitua pelo URL da página desejada
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.getElementById('fluxobt');
+
+    // Define o destino do link dinamicamente
+    link.href = 'manutencao.html'; // Substitua pelo URL da página desejada
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.getElementById('operacaobt');
+
+    // Define o destino do link dinamicamente
+    link.href = 'local.html'; // Substitua pelo URL da página desejada
+});
